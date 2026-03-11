465ba4e84ce319872ee485903d038e7dcd79386b
firdausnanda <firdausnanda46@gmail.com>
2026-03-11 16:47:47 +0700

    [FIX] Footer

--- src/components/DashboardFooter.jsx
+++ src/components/DashboardFooter.jsx
@@ -10,9 +10,9 @@ export default function DashboardFooter() {
             © 2026 KalaStudio. All rights reserved.
           </p>
           <div className="flex gap-6 text-xs text-slate-400 dark:text-slate-500 font-bold">
-            <Link className="hover:text-primary transition-colors" href="/syarat-ketentuan">Syarat & Ketentuan</Link>
-            <Link className="hover:text-primary transition-colors" href="/kebijakan-privasi">Kebijakan Privasi</Link>
-            <Link className="hover:text-primary transition-colors" href="/kontak">Bantuan</Link>
+            <Link target='_blank' className="hover:text-primary transition-colors" href="/syarat-ketentuan">Syarat & Ketentuan</Link>
+            <Link target='_blank' className="hover:text-primary transition-colors" href="/kebijakan-privasi">Kebijakan Privasi</Link>
+            <Link target='_blank' className="hover:text-primary transition-colors" href="/kontak">Bantuan</Link>
           </div>
         </div>
       </div>
